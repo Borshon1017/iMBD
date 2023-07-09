6be3ec6c8f159818a89c69d519fd5dc72b194c96
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2023-07-09 23:11:09 +0600

    ok

--- Models/database.php
+++ Models/database.php
@@ -2,7 +2,11 @@
 
 function dbConnection(){
 
+<<<<<<< Updated upstream
     $conn = mysqli_connect('localhost', 'root', '', 'iMBD');
+=======
+    $conn = mysqli_connect('localhost', 'root', '', 'imbd');
+>>>>>>> Stashed changes
     return $conn;
     
 }
