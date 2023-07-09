3ed1e7c7ef0dad9519207b2a61d90b0b0b0b4b0d
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2023-07-10 01:46:09 +0600

    minor changes

--- Models/database.php
+++ Models/database.php
@@ -2,8 +2,12 @@
 
 function dbConnection(){
 
+<<<<<<< Updated upstream
     $conn = mysqli_connect('localhost', 'root', '', 'imbd');
 
+=======
+    $conn = mysqli_connect('localhost', 'root', '', 'iMBD');
+>>>>>>> Stashed changes
     return $conn;
     
 }
