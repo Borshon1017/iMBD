6646791609e75cfb3059b7b8e22396258d113536
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2023-07-09 21:37:06 +0600

    Minor Updates

---
+++ Controllers/sign-in-controller.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once('../Models/user-info-model.php');
+
+    if(isset($_POST['submit'])){
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $rememberMe = $_POST['rememberMe'];
+
+        $status = login($email, $password);
+
+        if($status){
+
+            if(getRole() == "General User") header('location: ../index.html');
+            if(getRole() == "Administrator") header('location: ../index.html');
+            if(getRole() == "Content Writer") header('location: ../index.html');
+            if(getRole() == "Critic") header('location: ../index.html');
+
+        }else{
+            echo "invalid username/password";
+        }
+    }
+
+?>
--- Models/user-info-model.php
+++ Models/user-info-model.php
@@ -2,16 +2,28 @@
 
     require_once('database.php');
 
+    $row;
     function login($email, $password){
+
+        global $row;
 
         $con = dbConnection();
 
-        $sql = "select * from UserInfo where email ='{$email}' and password ='{$password}'";
+        $sql = "select * from UserInfo where email ='{$email}' and password ='{$password}' and status ='Active'";
         $result = mysqli_query($con, $sql);
         $count = mysqli_num_rows($result);
 
+        $row = mysqli_fetch_assoc($result);
+
         if($count == 1) return true;
         else return false;
+
+    }
+
+    function getRole(){
+
+        global $row;
+        return $row["Role"];
 
     }
 
@@ -19,7 +31,7 @@
 
         $con = dbConnection();
 
-        $sql = "insert into UserInfo values('', '{$fullname}' ,'{$username}' ,'{$phone}', '{$email}', '{$password}', '../Images/default_pfp.jpg', 'General User', 'Active')";
+        $sql = "insert into UserInfo values('', '{$fullname}' ,'{$username}' ,'{$phone}', '{$email}', '{$password}', 'Images/default_pfp.jpg', 'General User', 'Active')";
 
         if(mysqli_query($con, $sql)) return true;
         else return false;
