733cba455f5fce4b18e008317dec7fbf34441734
tanvir103 <tanvirh103@gmail.com>
2023-07-11 02:03:55 +0600

    row added

--- Views/upload.php
+++ Views/upload.php
@@ -1,26 +1,21 @@
 <?php
-<<<<<<< Updated upstream
-require_once('../Models/user-info-model.php');   
-$info = $_SESSION['info'];
-$id = $info['UserID'];
-=======
+    require_once('../Models/user-info-model.php');   
+    $id = $_SESSION['id'];
+    $src = $_FILES['myfile']['tmp_name'];
 
->>>>>>> Stashed changes
-$src = $_FILES['myfile']['tmp_name'];
+    $fileName = 'Uploads/Images/'.$_FILES['myfile']['name'];
+    $des = "../Uploads/Images/".$_FILES['myfile']['name'];
 
-$fileName = 'Uploads/Images/'.$_FILES['myfile']['name'];
-$des = "../Uploads/Images/".$_FILES['myfile']['name'];
-
-if(move_uploaded_file($src, $des)){
+    if(move_uploaded_file($src, $des)){
     echo "Success";
          
     $info = $_SESSION['info'];
     updateProfilePicture($fileName, $id);
 
-}
-else{
+    }
+    else{
     echo "Error";
-}
+    }
 
 
 ?>
