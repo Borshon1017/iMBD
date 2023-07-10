a72ab70ad5a8f07983a2594119895c1d661a63e0
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2023-07-11 01:33:58 +0600

    chan

--- Views/upload.php
+++ Views/upload.php
@@ -1,13 +1,22 @@
 <?php
+require_once('../Models/user-info-model.php');   
+$info = $_SESSION['info'];
+$id = $info['UserID'];
 $src = $_FILES['myfile']['tmp_name'];
 
-
-$des = "../Uploads/Images".$_FILES['myfile']['name'];
+$fileName = 'Uploads/Images/'.$_FILES['myfile']['name'];
+$des = "../Uploads/Images/".$_FILES['myfile']['name'];
 
 if(move_uploaded_file($src, $des)){
     echo "Success";
-}else{
+         
+    $info = $_SESSION['info'];
+    updateProfilePicture($fileName, $id);
+
+}
+else{
     echo "Error";
 }
+
 
 ?>
