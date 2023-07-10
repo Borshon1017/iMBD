175439717b40ece15950b2b1f2f5655cb4f4c91d
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2023-07-11 01:48:59 +0600

    minor changes

--- Views/upload.php
+++ Views/upload.php
@@ -1,7 +1,11 @@
 <?php
+<<<<<<< Updated upstream
 require_once('../Models/user-info-model.php');   
 $info = $_SESSION['info'];
 $id = $info['UserID'];
+=======
+
+>>>>>>> Stashed changes
 $src = $_FILES['myfile']['tmp_name'];
 
 $fileName = 'Uploads/Images/'.$_FILES['myfile']['name'];
