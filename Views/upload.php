ae2cb51413380be2bc3daf21859a301313c8632d
tanvir103 <tanvirh103@gmail.com>
2023-07-11 02:24:59 +0600

    minor

--- Views/upload.php
+++ Views/upload.php
@@ -1,17 +1,10 @@
 <?php
-<<<<<<< Updated upstream
     require_once('../Models/user-info-model.php');   
     $id = $_SESSION['id'];
     $src = $_FILES['myfile']['tmp_name'];
 
     $fileName = 'Uploads/Images/'.$_FILES['myfile']['name'];
     $des = "../Uploads/Images/".$_FILES['myfile']['name'];
-=======
-require_once('../Models/user-info-model.php');   
-$info = $_SESSION['info'];
-$id = $info['UserID'];
-$src = $_FILES['myfile']['tmp_name'];
->>>>>>> Stashed changes
 
     if(move_uploaded_file($src, $des)){
     echo "Success";
