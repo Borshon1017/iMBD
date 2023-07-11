cd93c98b64ba18102693df4f3f9f2992ead08409
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2023-07-11 23:30:59 +0600

    anime and Movies

--- Models/content-info-model.php
+++ Models/content-info-model.php
@@ -16,25 +16,115 @@ function showcontent($cid)
  
      
         $crow = mysqli_fetch_assoc($result);
+        $count = mysqli_num_rows($result);
+        if($count == 1) 
+        {
+            $posterURL = $crow['Poster'];
+            $title = $crow['Title'];
+            $description = $crow['ContentDescription'];
+            $releaseDate = $crow['ReleaseDate'];
+    
+            echo '<tr>';
+            echo '<td><img src="' . $posterURL . '" width="180px"></td>';
+            echo '<td valign="top" align="left">';
+            echo '<font color="white" face="times new roman" size="6">' . $title . '</font><br><br>';
+            echo '<font color="white" face="times new roman" size="4">' . $description . '</font><br><br>';
+            echo '<font color="white" face="times new roman" size="4">Release Date: ' . $releaseDate . '</font><br><br>';
+            echo '<a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>';
+            echo '</td>';
+            echo '</tr>';
+        
 
+        }
+        else
+        {
+
+        }
        
-        $posterURL = $crow['Poster'];
-        $title = $crow['Title'];
-        $description = $crow['ContentDescription'];
-        $releaseDate = $crow['ReleaseDate'];
+}
 
-        echo '<tr>';
-        echo '<td><img src="' . $posterURL . '" width="180px"></td>';
-        echo '<td valign="top" align="left">';
-        echo '<font color="white" face="times new roman" size="6">' . $title . '</font><br><br>';
-        echo '<font color="white" face="times new roman" size="4">' . $description . '</font><br><br>';
-        echo '<font color="white" face="times new roman" size="4">Release Date: ' . $releaseDate . '</font><br><br>';
-        echo '<a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>';
-        echo '</td>';
-        echo '</tr>';
+
+
+function showMovies($cid)
+{
+   
+  global $crow;
+  $con = dbConnection();
+
+   
+    $sql = "SELECT * FROM contentinfo WHERE ContentID='$cid' and Category='Movie' ";
+    $result = mysqli_query($con, $sql);
+
  
 
+     
+        $crow = mysqli_fetch_assoc($result);
+        $count = mysqli_num_rows($result);
+        if($count == 1) 
+        {
+            $posterURL = $crow['Poster'];
+            $title = $crow['Title'];
+            $description = $crow['ContentDescription'];
+            $releaseDate = $crow['ReleaseDate'];
+    
+            echo '<tr>';
+            echo '<td><img src="' . $posterURL . '" width="180px"></td>';
+            echo '<td valign="top" align="left">';
+            echo '<font color="white" face="times new roman" size="6">' . $title . '</font><br><br>';
+            echo '<font color="white" face="times new roman" size="4">' . $description . '</font><br><br>';
+            echo '<font color="white" face="times new roman" size="4">Release Date: ' . $releaseDate . '</font><br><br>';
+            echo '<a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>';
+            echo '</td>';
+            echo '</tr>';
+        
 
+        }
+        else
+        {
+
+        }
+       
+}
+
+function showAnime($cid)
+{
+   
+  global $crow;
+  $con = dbConnection();
+
+   
+    $sql = "SELECT * FROM contentinfo WHERE ContentID='$cid' and Category='Anime' ";
+    $result = mysqli_query($con, $sql);
+
+ 
+
+     
+        $crow = mysqli_fetch_assoc($result);
+        $count = mysqli_num_rows($result);
+        if($count == 1) 
+        {
+            $posterURL = $crow['Poster'];
+            $title = $crow['Title'];
+            $description = $crow['ContentDescription'];
+            $releaseDate = $crow['ReleaseDate'];
+    
+            echo '<tr>';
+            echo '<td><img src="' . $posterURL . '" width="180px"></td>';
+            echo '<td valign="top" align="left">';
+            echo '<font color="white" face="times new roman" size="6">' . $title . '</font><br><br>';
+            echo '<font color="white" face="times new roman" size="4">' . $description . '</font><br><br>';
+            echo '<font color="white" face="times new roman" size="4">Release Date: ' . $releaseDate . '</font><br><br>';
+            echo '<a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>';
+            echo '</td>';
+            echo '</tr>';
+        
+
+        }
+        else
+        {
+
+        }
+       
 }
 
 ?>
