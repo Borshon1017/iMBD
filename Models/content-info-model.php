af17eb2a936caed17e2a72466fee5b30a67b49ae
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2023-07-12 01:48:58 +0600

    Posters

--- Models/content-info-model.php
+++ Models/content-info-model.php
@@ -20,7 +20,7 @@ function showcontent($cid)
         if($count == 1) 
         {
             $posterURL = $crow['Poster'];
-            $title = $crow['Title'];
+            $ContentTitle = $crow['ContentTitle'];
             $description = $crow['ContentDescription'];
             $releaseDate = $crow['ReleaseDate'];
     
@@ -63,7 +63,7 @@ function showMovies($cid)
         if($count == 1) 
         {
             $posterURL = $crow['Poster'];
-            $title = $crow['Title'];
+            $title = $crow['ContentTitle'];
             $description = $crow['ContentDescription'];
             $releaseDate = $crow['ReleaseDate'];
     
@@ -104,7 +104,48 @@ function showAnime($cid)
         if($count == 1) 
         {
             $posterURL = $crow['Poster'];
-            $title = $crow['Title'];
+            $title = $crow['ContentTitle'];
+            $description = $crow['ContentDescription'];
+            $releaseDate = $crow['ReleaseDate'];
+    
+            echo '<tr>';
+            echo '<td><img src="' . $posterURL . '" width="180px"></td>';
+            echo '<td valign="top" align="left">';
+            echo '<font color="white" face="times new roman" size="6">' . $title . '</font><br><br>';
+            echo '<font color="white" face="times new roman" size="4">' . $description . '</font><br><br>';
+            echo '<font color="white" face="times new roman" size="4">Release Date: ' . $releaseDate . '</font><br><br>';
+            echo '<a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>';
+            echo '</td>';
+            echo '</tr>';
+        
+
+        }
+        else
+        {
+
+        }
+       
+}
+
+function showTVShow($cid)
+{
+   
+  global $crow;
+  $con = dbConnection();
+
+   
+    $sql = "SELECT * FROM contentinfo WHERE ContentID='$cid' and Category='TV Show' ";
+    $result = mysqli_query($con, $sql);
+
+ 
+
+     
+        $crow = mysqli_fetch_assoc($result);
+        $count = mysqli_num_rows($result);
+        if($count == 1) 
+        {
+            $posterURL = $crow['Poster'];
+            $title = $crow['ContentTitle'];
             $description = $crow['ContentDescription'];
             $releaseDate = $crow['ReleaseDate'];
     
--- index.php
+++ index.php
@@ -57,42 +57,11 @@
                 <a href=""><font color="5799EF" face="times new roman" size="4">More to explore</font></a><br><br>
                 <table width="90%" bgcolor="black" border="0" cellspacing="0" cellpadding="15">
     <?php
-for ($cid = 1; $cid <=2; $cid++) {
-showcontent($cid);
+for ($cid = 1; $cid <=6; $cid++) {
+showMovies($cid);
 }
 ?>
-                    <tr>
-                        <td>
-                            <img src="Uploads/posters/across_the_spider-verse.jpg" width="180px">
-                        </td>
-                        <td valign="top" align="left">
-                            <font color="white" face="times new roman" size="6">Spiderman: Across The Spider Verse</font><br><br>
-                            <font color="white" face="times new roman" size="4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse praesentium suscipit error! Saepe incidunt illo maxime vero veniam quos itaque praesentium natus similique porro, eaque autem? Esse corporis modi laboriosam?</font><br><br>
-                            <font color="white" face="times new roman" size="4">Release Date: 19/01/2002</font><br><br>
-                            <a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>
-                        </td>
-                    </tr>
-                        <td>
-                            <img src="Uploads/posters/Among_Us_poster.png" width="180px">
-                        </td>
-                        <td valign="top" align="left">
-                            <font color="white" face="times new roman" size="6">Among Us: Who Is The Imposter</font><br><br>
-                            <font color="white" face="times new roman" size="4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse praesentium suscipit error! Saepe incidunt illo maxime vero veniam quos itaque praesentium natus similique porro, eaque autem? Esse corporis modi laboriosam?</font><br><br>
-                            <font color="white" face="times new roman" size="4">Release Date: 19/01/2002</font><br><br>
-                            <a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="Uploads/posters/morbius.jpg" width="180px">
-                        </td>
-                        <td valign="top" align="left">
-                            <font color="white" face="times new roman" size="6">Morbius: It's Morbing Time</font><br><br>
-                            <font color="white" face="times new roman" size="4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse praesentium suscipit error! Saepe incidunt illo maxime vero veniam quos itaque praesentium natus similique porro, eaque autem? Esse corporis modi laboriosam?</font><br><br>
-                            <font color="white" face="times new roman" size="4">Release Date: 19/01/2002</font><br><br>
-                            <a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>
-                        </td>
-                    </tr>
+                   
                 </table><br><br><br>
 
                 <font color="F5C518" face="times new roman" size="12">TV Shows</font><br>
@@ -101,74 +70,26 @@ showcontent($cid);
                 <table width="90%" bgcolor="black" border="0" cellspacing="0" cellpadding="15">
                     <a href="sign-in.html"><tr>
                         <td>
-                            <img src="Uploads/posters/across_the_spider-verse.jpg" width="180px">
-                        </td>
-                        <td valign="top" align="left">
-                            <font color="white" face="times new roman" size="6">Spiderman: Across The Spider Verse</font><br><br>
-                            <font color="white" face="times new roman" size="4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse praesentium suscipit error! Saepe incidunt illo maxime vero veniam quos itaque praesentium natus similique porro, eaque autem? Esse corporis modi laboriosam?</font><br><br>
-                            <font color="white" face="times new roman" size="4">Release Date: 19/01/2002</font><br><br>
-                            <a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>
-                        </td>
-                    </tr></a>
-                        <td>
-                            <img src="Uploads/posters/Among_Us_poster.png" width="180px">
-                        </td>
-                        <td valign="top" align="left">
-                            <font color="white" face="times new roman" size="6">Among Us: Who Is The Imposter</font><br><br>
-                            <font color="white" face="times new roman" size="4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse praesentium suscipit error! Saepe incidunt illo maxime vero veniam quos itaque praesentium natus similique porro, eaque autem? Esse corporis modi laboriosam?</font><br><br>
-                            <font color="white" face="times new roman" size="4">Release Date: 19/01/2002</font><br><br>
-                            <a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="Uploads/posters/morbius.jpg" width="180px">
-                        </td>
-                        <td valign="top" align="left">
-                            <font color="white" face="times new roman" size="6">Morbius: It's Morbing Time</font><br><br>
-                            <font color="white" face="times new roman" size="4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse praesentium suscipit error! Saepe incidunt illo maxime vero veniam quos itaque praesentium natus similique porro, eaque autem? Esse corporis modi laboriosam?</font><br><br>
-                            <font color="white" face="times new roman" size="4">Release Date: 19/01/2002</font><br><br>
-                            <a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>
-                        </td>
-                    </tr>
+            
+                            <?php
+for ($cid = 1; $cid <=6; $cid++) {
+showTVShow($cid);
+}
+?>
+
                 </table><br><br><br>
                 
                 <font color="F5C518" face="times new roman" size="12">Animes</font><br>
                 <hr color="F5C518" width="530px" align="left"><br>
                 <a href=""><font color="5799EF" face="times new roman" size="4">More to explore</font></a><br><br>
                 <table width="90%" bgcolor="black" border="0" cellspacing="0" cellpadding="15">
-                    <a href="sign-in.html"><tr>
-                        <td>
-                            <img src="Uploads/posters/across_the_spider-verse.jpg" width="180px">
-                        </td>
-                        <td valign="top" align="left">
-                            <font color="white" face="times new roman" size="6">Spiderman: Across The Spider Verse</font><br><br>
-                            <font color="white" face="times new roman" size="4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse praesentium suscipit error! Saepe incidunt illo maxime vero veniam quos itaque praesentium natus similique porro, eaque autem? Esse corporis modi laboriosam?</font><br><br>
-                            <font color="white" face="times new roman" size="4">Release Date: 19/01/2002</font><br><br>
-                            <a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>
-                        </td>
-                    </tr></a>
-                        <td>
-                            <img src="Uploads/posters/Among_Us_poster.png" width="180px">
-                        </td>
-                        <td valign="top" align="left">
-                            <font color="white" face="times new roman" size="6">Among Us: Who Is The Imposter</font><br><br>
-                            <font color="white" face="times new roman" size="4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse praesentium suscipit error! Saepe incidunt illo maxime vero veniam quos itaque praesentium natus similique porro, eaque autem? Esse corporis modi laboriosam?</font><br><br>
-                            <font color="white" face="times new roman" size="4">Release Date: 19/01/2002</font><br><br>
-                            <a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="Uploads/posters/morbius.jpg" width="180px">
-                        </td>
-                        <td valign="top" align="left">
-                            <font color="white" face="times new roman" size="6">Morbius: It's Morbing Time</font><br><br>
-                            <font color="white" face="times new roman" size="4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse praesentium suscipit error! Saepe incidunt illo maxime vero veniam quos itaque praesentium natus similique porro, eaque autem? Esse corporis modi laboriosam?</font><br><br>
-                            <font color="white" face="times new roman" size="4">Release Date: 19/01/2002</font><br><br>
-                            <a href=""><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>
-                        </td>
-                    </tr>
+                    <a href="sign-in.html">
+                    <?php
+for ($cid = 1; $cid <=6; $cid++) {
+showAnime($cid);
+}
+?>    
+                   
                 </table><br><br><br>
 
             </td>
