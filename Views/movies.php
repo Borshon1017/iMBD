0696967738c94b73162186352ed1378925aca223
tanvir103 <tanvirh103@gmail.com>
2023-07-15 18:41:24 +0600

    .

--- Views/movies.php
+++ Views/movies.php
@@ -1,5 +1,4 @@
 <?php
-//session_start();
     require_once('../Models/user-info-model.php'); 
     require_once('../Models/content-info-model.php'); 
     require_once('../Controllers/message-controller.php');  
