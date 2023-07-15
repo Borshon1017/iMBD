2568524377f921975f0901f2c1dbd47e64633fa7
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2023-07-16 00:21:15 +0600

    now working

--- Models/content-info-model.php
+++ Models/content-info-model.php
@@ -606,6 +606,63 @@ function showUploadsEdit($id, $site)
 }
 
 
+function showUploadsDelete($id, $site)
+{
+    global $crow;
+    $con = dbConnection();
+    $row = UserInfo($id);
+    $userid= $row['UserID'] ;
+
+    $sql = "SELECT * FROM contentinfo WHERE UserID='$userid'";
+    $result = mysqli_query($con, $sql);
+
+    while ($crow = mysqli_fetch_assoc($result)) {
+        $cid = $crow['ContentID'];
+        $posterURL = $crow['Poster'];
+        $title = $crow['ContentTitle'];
+        $description = $crow['ContentDescription'];
+        $releaseDate = $crow['ReleaseDate'];
+
+        if (strlen($description) > 220) {
+            $description = substr($description, 0, 220) . '...';
+        }
+
+        echo '<tr>';
+        if ($site == "index") {
+            echo '<td><a href="views/content-page.php?cid=' . $cid . '"><img src="' . $posterURL . '" width="180px"></a></td>';
+        } else if ($site == "view") {
+            echo '<td><a href="../views/content-page.php?cid=' . $cid . '"><img src="../' . $posterURL . '" width="180px"></a></td>';
+        }
+        echo '<td valign="top" align="left">';
+        if ($site == "index") {
+            echo '<a href="views/content-page.php?cid=' . $cid . '"> <font color="white" face="times new roman" size="6">' . $title . '</font></a><br><br>';
+        } else if ($site == "view") {
+            echo '<a href="../views/content-page.php?cid=' . $cid . '"> <font color="white" face="times new roman" size="6">' . $title . '</font></a><br><br>';
+        }
+        echo '<font color="white" face="times new roman" size="4">' . $description . '</font><br><br>';
+        echo '<font color="white" face="times new roman" size="4">Release Date: ' . $releaseDate . '</font><br><br>';
+        if (isset($_COOKIE['flag'])) {
+            $row = UserInfo($id);
+            if ($row['Role'] == "General User") {
+                $sql = "SELECT * FROM watchlist WHERE UserID = '$id' AND ContentID = '$cid'";
+                $result = mysqli_query($con, $sql);
+                $count = mysqli_num_rows($result);
+                if ($count > 0) {
+                    echo '<font color="5799EF" face="times new roman" size="4">Already added to Watchlist</font><br><br>';
+                } else {
+                    echo '<a href="Controllers/Add-to-Watchlist.php?cid=' . $cid . '"><font color="5799EF" face="times new roman" size="4">Add to Watchlist</font></a><br><br>';
+                }
+            }
+            echo '<form action="../Controllers/delete-content-details.php" method="POST" enctype="multipart/form-data">';
+            echo '<input type="hidden" name="cid" value="' . $cid . '">';
+            echo '<input type="submit" value="Delete Content">';
+            echo '</form>';
+        }
+        echo '</td>';
+        echo '</tr>';
+    }
+}
+
 
 
 ?>
