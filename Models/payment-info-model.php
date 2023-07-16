51953cefba9ea9db894fcf656bb63d603b4421b0
tanvir103 <tanvirh103@gmail.com>
2023-07-16 19:14:52 +0600

    .

--- Models/payment-info-model.php
+++ Models/payment-info-model.php
@@ -21,51 +21,8 @@ function getAllPaymentInfo()
     $result = mysqli_query($con, $sql);
     return $result;
 }
+ 
 
-function showPaymentInfoByID()
-{
-    $userID = $_COOKIE['id'];
-    $paymentInfo = getPaymentInfoByUserID($userID);
-
-    echo '<table width="85%" bgcolor="black" border="0" cellspacing="0" cellpadding="15">';
-    echo '<tr>';
-
-    echo '<td>';
-    echo '<font color="F5C518" face="times new roman" size="5">Content Title</font>';
-    echo '<hr color="F5C518" width="150px" align="left">';
-    echo '</td>';
-    echo '<td>';
-    echo '<font color="F5C518" face="times new roman" size="5">Price</font>';
-    echo '<hr color="F5C518" width="75px" align="left">';
-    echo '</td>';
-    echo '<td>';
-    echo '<font color="F5C518" face="times new roman" size="5">Purchase Date</font>';
-    echo '<hr color="F5C518" width="160px" align="left">';
-    echo '</td>';
-    echo '</tr>';
-
-    while ($row = mysqli_fetch_assoc($paymentInfo)) 
-    {
-        
-        $contentTitle = $row['ContentTitle'];
-        $price = $row['Price'];
-        $purchaseDate = $row['PurchaseDate'];
-
-        echo '<tr>';
-        echo '<td>';
-        echo '<font color="white" face="times new roman" size="5">' . $contentTitle . '</font>';
-        echo '</td>';
-        echo '<td>';
-        echo '<font color="white" face="times new roman" size="5">' . $price . '</font>';
-        echo '</td>';
-        echo '<td>';
-        echo '<font color="white" face="times new roman" size="5">' . $purchaseDate . '</font>';
-        echo '</td>';
-        echo '</tr>';
-    }
-
-    echo '</table>';
-}
 
 function showAllPaymentInfo()
 {
--- Views/purchase-history.php
+++ Views/purchase-history.php
@@ -6,10 +6,8 @@
     }
     $id=$_COOKIE['id'];
     $row=UserInfo($id);
-
+    $paymentInfo = getPaymentInfoByUserID($id);
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -46,12 +44,53 @@
     <center>
         <font color="F5C518" face="times new roman" size="12">Purchase History</font><br><br><br>
         <hr color="F5C518" width="530px"><br><br><br>
-<?php
-        showPaymentInfoByID();
+        <?php
+            if(mysqli_num_rows($paymentInfo)>0){
+                echo "
+                <table width=\"85%\" bgcolor=\"black\" border=\"0\" cellspacing=\"0\" cellpadding=\"15\">
+                <tr>
+                <td>
+                <font color=\"F5C518\" face=\"times new roman\" size=\"5\">Content Title</font>;
+                <hr color=\"F5C518\" width=\"150px\" align=\"left\">
+                </td>
+                <td>
+                <font color=\"F5C518\" face=\"times new roman\" size=\"5\">Price</font>;
+                <hr color=\"F5C518\" width=\"75px\" align=\"left\">
+                </td>
+                <td>
+                <font color=\"F5C518\" face=\"times new roman\" size=\"5\">Purchase Date</font>;
+                <hr color=\"F5C518\" width=\"160px\" align=\"left\">;
+                </td>
+                </tr>";
+
+                 while ($row = mysqli_fetch_assoc($paymentInfo)) 
+                {
+        
+                $contentTitle = $row['ContentTitle'];
+                $price = $row['Price'];
+                $purchaseDate = $row['PurchaseDate'];
+                    echo"
+                    <tr>
+                    <td>
+                    <font color=\"white\" face=\"times new roman\" size=\"5\">$contentTitle</font>
+                    </td>
+                    <td>
+                    <font color=\"white\" face=\"times new roman\" size=\"5\">$price</font>
+                    </td>
+                    <td>
+                    <font color=\"white\" face=\"times new roman\" size=\"5\">$purchaseDate</font>
+                    </td>
+                    </tr>";
+                }
+            }else{
+                echo"<tr><td align=\"center\"><font color=\"white\" face=\"times new roman\" size=\"6\">No Purchase History Found</font></td></tr>";
+            }
         ?>
-        <br><br><br>
-    </center>
+        </table>
+     
     <br><br><br>
+    </center>
+    <br><br><br><br><br><br>
     <center>
         <a href="about-us.php"><font color="white" face="times new roman" size="4">About Us</font></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="helpline.php"><font color="white" face="times new roman" size="4">Helpline</font></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
