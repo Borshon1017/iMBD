fc6096f8628782355bba3795bf2c5f8f7e690077
tanvir103 <tanvirh103@gmail.com>
2023-07-31 02:40:37 +0600

    .

--- Views/search-user.php
+++ Views/search-user.php
@@ -14,6 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
     <title>iMBD View All Users</title>
 </head>
 <body bgcolor="black">
