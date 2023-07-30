b76d959c7fb680a8eaa3c4f68fcfd5fe49fa853a
Rianul Amin <87932192+ppsppspsspss@users.noreply.github.com>
2023-07-31 02:19:30 +0600

    minor changes

--- Views/search-user.php
+++ Views/search-user.php
@@ -17,16 +17,19 @@
     <title>iMBD View All Users</title>
 </head>
 <body bgcolor="black">
-    <table width="100%" bgcolor="black" border="0" cellspacing="0" cellpadding="15">
+    <table width="100%" border="0" cellspacing="0" cellpadding="10">
         <tr height="60px">
             <td>
                 &nbsp;<a href="../index.php"><img src="../Uploads/logo.png" width="80px"></a>
             </td>
+            <td></td>
+            <td></td>
             <td>
-                <form action="search-content.php" method="post">
-                <input type="text" name="title" placeholder="Search iMBD" size="100px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <input type="submit" name="submit" value="Search">
-                </form>
+                
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="search-content.php"><button class="btn search">Search iMBD</button></a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
             </td>
             <td>
                 <img src="../<?php echo $row['ProfilePicture']; ?>" width="40px">&nbsp;&nbsp;&nbsp;
@@ -38,6 +41,7 @@
                     <option value="logout-page.php">Log Out</option>
                 </select>
             </td>
+            <td></td>
         </tr>
     </table><br><br><br>
     
