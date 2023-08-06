7c0c20cee203baffb40dd0419b0ee0ea9d32b4a7
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2023-08-06 11:18:52 +0600

    deletecont

--- Views/delete-content.php
+++ Views/delete-content.php
@@ -75,10 +75,13 @@
             xhttp.open('post', '../Controllers/search-to-delete-controller.php', true);
             xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             xhttp.send('name=' + str);
-            xhttp.onload = function () {
-                document.getElementById('message').innerHTML = this.responseText;
-            }
+            xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            document.getElementById('message').innerHTML = this.responseText;
         }
+            
+        }
+    }
         
       
         window.onload = function () {
